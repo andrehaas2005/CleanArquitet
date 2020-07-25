8571a122150cf6bd8bc9f08c8161f7b195a8a7e4
Andre <andre.haas@dextra-sw.com>
2020-07-25 16:24:56 -0300

    Initial Commit

---
+++ CleanApp/CleanApp.h
@@ -0,0 +1,19 @@
+//
+//  CleanApp.h
+//  CleanApp
+//
+//  Created by André Haas on 25/07/20.
+//  Copyright © 2020 André Haas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CleanApp.
+FOUNDATION_EXPORT double CleanAppVersionNumber;
+
+//! Project version string for CleanApp.
+FOUNDATION_EXPORT const unsigned char CleanAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanApp/PublicHeader.h>
+
+
