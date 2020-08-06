1f23f39741f6366b3bfe9276794203db222347d3
andre.haas.dex <andre.haas@dextra-sw.com>
2020-08-06 11:14:20 -0300

    Add UI Layer

---
+++ UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,18 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by André Haas on 06/08/20.
+//  Copyright © 2020 André Haas. All rights reserved.
+//
+
+import XCTest
+
+class SignUpViewControllerTests: XCTestCase {
+
+
+    func test_() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
