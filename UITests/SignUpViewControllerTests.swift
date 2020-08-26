d0b64685c02f61d3468c512fcc42123a3f1a565e
andre.haas.dex <andre.haas@dextra-sw.com>
2020-08-26 17:41:00 -0300

    ensure SignUpViewController calls signUp with correct values

--- UITests/SignUpViewControllerTests.swift
+++ UITests/SignUpViewControllerTests.swift
@@ -26,16 +26,18 @@ class SignUpViewControllerTests: XCTestCase {
     }
 
     func test_saveButton_calls_signUp_on_tap() {
-        var callsCount = 0
-//        let signUpSpy: (SignUpViewModel) -> Void = { _ in
-//            callsCount += 1
-//        }
-        let sut = makeSuv(signUpSpy: { _ in
-            callsCount += 1
-        })
+        var signUpViewModel: SignUpViewModel?
+        let sut = makeSuv(signUpSpy: { signUpViewModel = $0 })
+        let name = sut.nameTextField?.text
+        let email = sut.emailTextField?.text
+        let password = sut.passwordTextField?.text
+        let passwordConformation = sut.passwordConfirmationTextField?.text
 
         sut.saveButton?.simulateTap()
-        XCTAssertEqual(callsCount, 1)
+        XCTAssertEqual(signUpViewModel, SignUpViewModel(name: name,
+                                                        email: email,
+                                                        password: password,
+                                                        passwordConfirmation: passwordConformation))
     }
 }
 
