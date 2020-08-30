4b44bfab562ee42e531ee3328a108d5fbb9b3d86
André Haas <andrehaas@005@gmail.com>
2020-08-30 19:33:17 -0300

    add main target

---
+++ Main/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Main
+//
+//  Created by André Haas on 30/08/20.
+//  Copyright © 2020 André Haas. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
+
---
+++ Main/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  Main
+//
+//  Created by André Haas on 30/08/20.
+//  Copyright © 2020 André Haas. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
