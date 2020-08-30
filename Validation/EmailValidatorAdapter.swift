83245cd44b0aa36ebfa4abd33a6fcd94a0739e26
André Haas <andrehaas@005@gmail.com>
2020-08-30 19:25:52 -0300

    creste class file EmailValidatorAdapter

---
+++ Validation/EmailValidatorAdapter.swift
@@ -0,0 +1,20 @@
+//
+//  EmailValidatorAdapter.swift
+//  Validation
+//
+//  Created by André Haas on 30/08/20.
+//  Copyright © 2020 André Haas. All rights reserved.
+//
+
+import Foundation
+import Presentation
+
+public final class EmailValidatorAdapter: EmailValidator {
+    private let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    public init() { }
+    public func isValid(email: String) -> Bool {
+        let range = NSRange(location: 0, length: email.utf16.count)
+        let regex = try! NSRegularExpression(pattern: pattern)
+        return regex.firstMatch(in: email, options: [], range: range) != nil
+    }
+}
