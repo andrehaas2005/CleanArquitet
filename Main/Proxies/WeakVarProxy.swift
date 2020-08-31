ba161353d908e7e9879af40278e100a255139ce0
André Haas <andrehaas@005@gmail.com>
2020-08-30 21:05:03 -0300

    move WeakVarProxy for new file

---
+++ Main/Proxies/WeakVarProxy.swift
@@ -0,0 +1,29 @@
+//
+//  WeakVarProxy.swift
+//  Main
+//
+//  Created by André Haas on 30/08/20.
+//  Copyright © 2020 André Haas. All rights reserved.
+//
+
+import Foundation
+import Presentation
+
+final class WeakVarProxy<T: AnyObject> {
+    private weak var instance: T?
+    init(_ instance: T) {
+        self.instance = instance
+    }
+}
+
+extension WeakVarProxy: AlertView where T: AlertView {
+    func showMessage(viewModel: AlertViewModel) {
+        instance?.showMessage(viewModel: viewModel)
+    }
+}
+
+extension WeakVarProxy: LoadingView where T: LoadingView {
+    func display(viewModel: LoadingViewModel) {
+        instance?.display(viewModel: viewModel)
+    }
+}
