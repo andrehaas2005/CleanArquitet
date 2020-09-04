cedfef96a5f7064a6204172fe84847a4a2383568
André Haas <andrehaas@005@gmail.com>
2020-09-04 15:08:09 -0300

    Add AuthenticationModel

---
+++ Domain/Authentication.swift
@@ -0,0 +1,24 @@
+//
+//  AddAccount.swift
+//  Domain
+//
+//  Created by André Haas on 25/07/20.
+//  Copyright © 2020 André Haas. All rights reserved.
+//
+
+import Foundation
+
+public protocol Authentication {
+    typealias Result = Swift.Result<AccountModel, DomainError>
+    func add(authenticationModel: AuthenticationModel, completion: @escaping (Result) -> Void)
+}
+
+public struct AuthenticationModel: Model {
+public var email: String
+    public var password: String
+
+   public init(email: String, password: String) {
+        self.email = email
+        self.password = password
+    }
+}
